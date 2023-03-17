81f987f5dffd463ff5cc1e7f6641c65aec92ae9e
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-03-17 21:45:22 +0200

    innit

--- src/main/java/Module13/Main.java
+++ src/main/java/Module13/Main.java
@@ -1,4 +1,27 @@
 package Module13;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
+    public static final String USERS = "https://jsonplaceholder.typicode.com";
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        User user = new User();
+        user.setId(1);
+        user.setName("Oleksandr");
+        user.setUserName("Oleksandr11");
+        user.setEmail("rogozkin371@gmail.com");
+        User userCreate = Utils.createNewObject(user, URI.create(USERS));
+        System.out.println(userCreate);
+    }
+
+
+
 }
--- src/main/java/Module13/Utils.java
+++ src/main/java/Module13/Utils.java
@@ -1,6 +1,44 @@
 package Module13;
 
-public class Utils {
+import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Utils {
+    private static final HttpClient CLIENT = HttpClient.newHttpClient();
+    private static final Gson GSON = new Gson();
+    public static User createNewObject(User user, URI uri) throws IOException, InterruptedException {
+        String restructuredBody=GSON.toJson(user);
+        HttpRequest request=HttpRequest.newBuilder()
+                .uri(URI.create(uri+"/users"))
+                .POST(HttpRequest.BodyPublishers.ofString(restructuredBody))
+                .header("Content-type", "application/json")
+                .build();
+        HttpResponse<String> response =
+                CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("POST " + response.statusCode());
+
+        return GSON.fromJson(response.body(),User.class) ;
+
+    }
+    //    public static renewObject(){
+//
+//    }
+//    public static deleteObject(){
+//
+//    }
+//    public static getInformationAboutAllObject(){
+//
+//    }
+//    public static getInformationAboutObjectFromId(){
+//
+//    }
+//    public static getInformationAboutObjectFromName(){
+//
+//    }
 
 }
